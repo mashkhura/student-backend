86de0cf92064bf10efcc79eb82525cd60c6de280
mashkhura <72493726+mashkhura@users.noreply.github.com>
2024-05-03 09:44:37 +0000

    studentBackend

--- blockchainTrial-done/src/config/config.ts
+++ blockchainTrial-done/src/config/config.ts
@@ -5,7 +5,7 @@ const algodToken = "a".repeat(64);
 const server: string = "http://localhost";
 const port: string = "4001";
 const mnemonic: string = 
-"profit habit library minimum sorry elder vocal globe magic expose tourist food project peanut frown usual convince donate shuffle alter camp jeans brother absent senior"
+"friend mix basket journey sad forest predict kit tilt aspect short option cat inflict million mango allow catalog grab omit clip anxiety business absent rely"
 
 export function getClient(): algosdk.Algodv2 {
     
